fdc998d6166798c15fa4017249fdcb7ed43599df
Musika Iqfat <musfikaiqfatmomo21.com>
2025-09-16 00:03:26 +0600

    authenticated users

--- src/app/(root)/all-flowers/[id]/page.jsx
+++ src/app/(root)/all-flowers/[id]/page.jsx
@@ -1,11 +1,11 @@
 import dbConnect, { collectionsNameObj } from '@/lib/dbConnect';
-import { ObjectId } from 'mongodb';
+// import { ObjectId } from 'mongodb';
 import React from 'react'
 
 export default async function FlowerDetailsPage({ params }) {
     const p = await params;
     const flowerCollection = dbConnect(collectionsNameObj.flowerCollection);
-    const data = await flowerCollection.findOne({_id: new ObjectId(p.id)})
+    // const data = await flowerCollection.findOne({_id: new ObjectId(p.id)})
     return (
         <div>
             Details
